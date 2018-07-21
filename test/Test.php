6aebbbff339a362ad8a5933b9a4741be2bd02132
liyunzhen <liyunzhen@360.com>
2018-07-21 12:18:43 +0800

    fix description

--- test/Test.php
+++ test/Test.php
@@ -23,20 +23,110 @@ fclose($file);
 //建立TrieTree
 $svc = new \App\Service\RiskWordFilterService;
 $wordTrieTree  = $svc->buildWordTree($wordArr);
-
-$title = '中国和德国踢足球';
-$filterWords = $svc->searchWord($title);
+var_dump(json_encode($wordTrieTree));
 /*
-array(2) {
-  ["中国"]=>
-  string(6) "中国"
-  ["德国,足球"]=>
-  array(2) {
-    [0]=>
-    string(6) "德国"
-    [1]=>
-    string(6) "足球"
-  }
+{
+    "中": {
+        "child": {
+            "国": {
+                "child": [],
+                "is_end": true,
+                "word": "中国",
+                "relate": [
+                    []
+                ]
+            }
+        },
+        "is_end": false
+    },
+    "阿": {
+        "child": {
+            "根": {
+                "child": {
+                    "廷": {
+                        "child": [],
+                        "is_end": true,
+                        "word": "阿根廷",
+                        "relate": [
+                            []
+                        ]
+                    }
+                },
+                "is_end": false
+            }
+        },
+        "is_end": false
+    },
+    "足": {
+        "child": {
+            "球": {
+                "child": [],
+                "is_end": true,
+                "word": "足球",
+                "relate": [
+                    {
+                        "1": "世界杯"
+                    },
+                    {
+                        "1": "德国"
+                    }
+                ]
+            }
+        },
+        "is_end": false
+    },
+    "世": {
+        "child": {
+            "界": {
+                "child": {
+                    "杯": {
+                        "child": [],
+                        "is_end": true,
+                        "word": "世界杯",
+                        "relate": [
+                            [
+                                "足球"
+                            ]
+                        ]
+                    }
+                },
+                "is_end": false
+            }
+        },
+        "is_end": false
+    },
+    "德": {
+        "child": {
+            "国": {
+                "child": [],
+                "is_end": true,
+                "word": "德国",
+                "relate": [
+                    [
+                        "足球"
+                    ]
+                ]
+            }
+        },
+        "is_end": false
+    }
 }
 */
-var_dump($filterWords);exit;
+
+$title = '中国和德国踢足球，争夺世界杯';
+$filterWords = $svc->searchWord($title);
+var_dump(json_encode($filterWords));exit;
+/*
+output:
+{
+    "中国": "中国",
+    "德国,足球": [
+        "德国",
+        "足球"
+    ],
+    "世界杯,足球": [
+        "世界杯",
+        "足球"
+    ]
+}
+*/
